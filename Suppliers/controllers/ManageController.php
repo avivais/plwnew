749fc85c603628e2560a5214ad9a655d14cacc70
Avi Vaisenberger <avi@fabulagame.com>
2013-04-20 03:26:50 +0300

    Added edit and new actions

--- Suppliers/controllers/ManageController.php
+++ Suppliers/controllers/ManageController.php
@@ -1,11 +1,34 @@
 <?php
-	class Plw_Suppliers_ManageController extends Mage_Adminhtml_Controller_Action
-	{
-		public function indexAction()
-		{
-			$this->loadLayout()
-				->_setActiveMenu('suppliers/items');
-			$this->renderLayout();
-		}
-	}
+    class Plw_Suppliers_ManageController extends Mage_Adminhtml_Controller_Action {
+        public function indexAction() {
+            $this->loadLayout()
+                ->_setActiveMenu('suppliers/items');
+            $this->renderLayout();
+        }
+
+        public function editAction() {
+            $id = $this->getRequest()->getParam('id');
+            $model = Mage::getModel('suppliers/suppliers')->load($id);
+            if ($model->getId() || $id == 0) {
+                $data = Mage::getSingleton('adminhtml/session')->getFormData(true);
+                if (!empty($data)) {
+                    $model->setData($data);
+                }
+                Mage::register('suppliers_data', $model);
+                $this->loadLayout();
+                $this->_setActiveMenu('suppliers/items');
+                $this->getLayout()->getBlock('head')->setCanLoadExtJs(true);
+                //$this->_addContent($this->getLayout()->createBlock('suppliers/manage_edit'));
+                //->_addLeft($this->getLayout()->createBlock('suppliers/adminhtml_suppliers_edit_tabs'));
+                $this->renderLayout();
+            } else {
+                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('suppliers')->__('Item does not exist'));
+                $this->_redirect('*/*/');
+            }
+        }
+
+        public function newAction() {
+            $this->_forward('edit');
+        }
+    }
 
